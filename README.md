4967abe2c369851dc92755bbf2b8c889986bd223
omerbak <42501059+omerbak@users.noreply.github.com>
2023-04-05 15:37:26 +0300

    almost ready

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Portofolio_website
-This is a simple portofolio website to show some of my projects.
+This is a my portofolio website to show some of my projects.
+
+# thing to add or improve
+
+- add animations using **anime.js**
+- add container to all section for better alignment
+- make seperate cards for every required section 
+- change website colors
+- improve project card animation and display
+- add form instead of email
--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
-
+const sidebarTitle = document.querySelector(".sidebar-title")
+const links = document.querySelectorAll(".links li");
+const body = document.querySelector("body")
+console.log(body)
 toggleBtn.addEventListener("click", function () {
   // if (sidebar.classList.contains("show-sidebar")) {
   //   sidebar.classList.remove("show-sidebar");
   // } else {
   //   sidebar.classList.add("show-sidebar");
   // }
+  sidebarTitle.classList.toggle("active");
   console.log("toggle-clicked")
   sidebar.classList.toggle("show-sidebar");
+  body.classList.toggle("show-sidebar")
 });
 
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
-});
-
-console.log(toggleBtn);
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    sidebarTitle.classList.toggle("active");
+    console.log("toggle-clicked")
+    sidebar.classList.toggle("show-sidebar");
+    body.classList.toggle("show-sidebar")
+  })
+})
